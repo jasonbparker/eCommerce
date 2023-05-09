6de234aa304b8932cc0cc10f3c729b3bcb5d1c08
Jason Parker <Jbparker243@gmail.com>
2023-05-09 12:33:13 -0700

    added a few more assets, renamed store

--- src/products.js
+++ src/products.js
@@ -3,6 +3,10 @@ import product2 from "./assets/camera.png";
 import product3 from "./assets/laptop.png";
 import product4 from "./assets/shirt.png";
 import product5 from "./assets/5.png";
+import product6 from "./assets/6.webp";
+import product7 from "./assets/7.webp";
+import product8 from "./assets/8.webp";
+import product9 from "./assets/chair.avif";
 
 export const PRODUCTS = [
   {
@@ -13,26 +17,50 @@ export const PRODUCTS = [
   },
   {
     id: 1,
-    productName: "camera",
+    productName: "Camera",
     price: 399.0,
     productImage: product2,
   },
   {
     id: 2,
-    productName: "laptop",
+    productName: "Laptop",
     price: 1399.0,
     productImage: product3,
   },
   {
     id: 3,
-    productName: "shirt",
+    productName: "Denim Shirt",
     price: 99.0,
     productImage: product4,
   },
   {
     id: 4,
-    productName: "led",
+    productName: "LED Strip Lights",
     price: 79.0,
     productImage: product5,
+  },
+  {
+    id: 5,
+    productName: "Don't Trip Shirt",
+    price: 25.0,
+    productImage: product6,
+  },
+  {
+    id: 6,
+    productName: "Golf Shirt",
+    price: 19.0,
+    productImage: product7,
+  },
+  {
+    id: 7,
+    productName: "Hat",
+    price: 14.0,
+    productImage: product8,
+  },
+  {
+    id: 8,
+    productName: "chair",
+    price: 20.0,
+    productImage: product9,
   },
 ];
